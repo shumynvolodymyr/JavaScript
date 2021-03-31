fb5974d204e0027a174ecc9319bfb4979ee96985
shumynvolodymyr <shumivova@gmail.com>
2021-03-31 15:21:58 +0300

    ClassWork 3

--- ClassWork_3/script.js
+++ ClassWork_3/script.js
@@ -54,55 +54,114 @@ for (let i = 0; i < elementLiColor.length; i++) {
 
 // h) отримує всі елементи 'a' та додає їм клас element_XXX. Де XXX - текстовий контент елементу a
 
-const allElementA = document.getElementsByTagName('a');
-for (let i = 0; i < allElementA.length; i++) {
-    allElementA[i].classList.add(`element_xxx`)
-    // element_xxx = 'allElementA[i].innerText';
+// const allElementA = document.getElementsByTagName('a');
+// for (const allElementAElement of allElementA) {
+//     allElementAElement.classList.add(`element_${allElementAElement.innerText}`);
+// }
 
-}
+// або через фор
+// const allElementA = document.getElementsByTagName('a');
+// for (let i = 0; i < allElementA.length; i++) {
+//     allElementA[i].classList.add(`element_${allElementA[i].innerText}`);
+// }
 
 // i) отримує всі елементи 'sub-header' та змінює колір фону. Фон отримати з prompt()
+
+// const allSubHeader = document.getElementsByClassName('sub-header');
+// for (let i = 0; i < allSubHeader.length; i++) {
+//     const allSubHeaderElement = allSubHeader[i];
+//     allSubHeaderElement.style.background = prompt('color');
+// }
+
 // j) отримує всі елементи 'sub-header' та змінює колір тексту у видаку якщо текст елемнту = content 2 segment . Колір отримати з prompt()
+
+// const allSubHeader = document.getElementsByClassName('sub-header');
+// for (let i = 0; i < allSubHeader.length; i++) {
+//     const allSubHeaderElement = allSubHeader[i];
+//     if (allSubHeader[i].innerText === 'content 2 segment') {
+//         allSubHeaderElement.style.background = prompt('color');
+//     }
+// }
+
 // k) отримує елемент з класом content_1 та заміняє  в ньому тест на довільний. Текст отримати з prompt()
+
+// const contentOneText = document.getElementsByClassName('content_1');
+// for (let i = 0; i < contentOneText.length; i++) {
+//     const contentOneTextElement = contentOneText[i];
+//     contentOneTextElement.innerText = prompt('Text');
+// }
+
 // l) отримати елементи p та змінити їм padding на 20px
+
+// const allP = document.getElementsByTagName('p');
+// for (let i = 0; i < allP.length; i++) {
+//     const allPElement = allP[i];
+//     allPElement.style.padding = '20px';
+//
+// }
+
 // m) отримати елементи з класом text2 та змінити їм текст на назву групи (mon-year)
+
+// const elementTextTwo = document.getElementsByClassName('text2');
+// for (const elementTextTwoElement of elementTextTwo) {
+//     elementTextTwoElement.innerText = 'February 2021';
+// }
 //
 // 2) Є масив котрий характеризує правила.
 //     Створити скрипт який ітерує цей масив, та робить кожне правило в окремому блоці.
 //     При цому в блоці, номер правила записати в свій блок, текст правила записати в свій окремий блок.
 //     Результатом відпрацювання скріпта повинна бути така ж структура яка міститься в файлі rules.html
 //
-// let rules = [
-//     {
-//         title: 'Первое правило Бойцовского клуба.',
-//         body: 'Никому не рассказывать о Бойцовском клубе.'
-//     },
-//     {
-//         title: 'Второе правило Бойцовского клуба.',
-//         body: 'Никогда никому не рассказывать о Бойцовском клубе.'
-//     },
-//     {
-//         title: 'Третье правило Бойцовского клуба.',
-//         body: 'В схватке участвуют только двое.'
-//     },
-//     {
-//         title: 'Четвертое правило Бойцовского клуба.',
-//         body: 'Не более одного поединка за один раз.'
-//     },
-//     {
-//         title: 'Пятое правило Бойцовского клуба.',
-//         body: 'Бойцы сражаются без обуви и голые по пояс.'
-//     },
-//     {
-//         title: 'Шестое правило Бойцовского клуба.',
-//         body: 'Поединок продолжается столько, сколько потребуется.'
-//     },
-//     {
-//         title: 'Седьмое правило Бойцовского клуба.',
-//         body: 'Если противник потерял сознание или делает вид, что потерял, или говорит «Хватит» — поединок окончен.'
-//     },
-//     {
-//         title: 'Восьмое и последнее правило Бойцовского клуба.',
-//         body: 'Новичок обязан принять бой.'
-//     }
-// ];
+let rules = [
+    {
+        title: 'Первое правило Бойцовского клуба.',
+        body: 'Никому не рассказывать о Бойцовском клубе.'
+    },
+    {
+        title: 'Второе правило Бойцовского клуба.',
+        body: 'Никогда никому не рассказывать о Бойцовском клубе.'
+    },
+    {
+        title: 'Третье правило Бойцовского клуба.',
+        body: 'В схватке участвуют только двое.'
+    },
+    {
+        title: 'Четвертое правило Бойцовского клуба.',
+        body: 'Не более одного поединка за один раз.'
+    },
+    {
+        title: 'Пятое правило Бойцовского клуба.',
+        body: 'Бойцы сражаются без обуви и голые по пояс.'
+    },
+    {
+        title: 'Шестое правило Бойцовского клуба.',
+        body: 'Поединок продолжается столько, сколько потребуется.'
+    },
+    {
+        title: 'Седьмое правило Бойцовского клуба.',
+        body: 'Если противник потерял сознание или делает вид, что потерял, или говорит «Хватит» — поединок окончен.'
+    },
+    {
+        title: 'Восьмое и последнее правило Бойцовского клуба.',
+        body: 'Новичок обязан принять бой.'
+    }
+];
+const divochka = document.createElement('div');
+divochka.innerHTML = `<h1>Правила бойцовского клуба<h1>`;
+document.body.appendChild(divochka);
+
+const content = document.createElement(`div`);
+divochka.appendChild(content);
+content.setAttribute(`id`, `wrap`);
+
+for (let i = 0; i < rules.length; i++) {
+let rulesDiv = document.createElement('div');
+    rulesDiv.innerHTML = `<h2>${rules[i].title}</h2>`;
+    rulesDiv.classList.add(`rules`, `rule${i+1}`);
+
+content.appendChild(rulesDiv);
+
+let rulesP = document.createElement('p');
+rulesP.innerHTML = `${rules[i].body}`;
+rulesDiv.appendChild(rulesP);
+}
