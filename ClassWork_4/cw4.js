ef5eb28fa6e8b5b712bae7dbd1c1aec24f9ae939
shumynvolodymyr <shumivova@gmail.com>
2021-04-04 19:35:58 +0300

    ClassWork_4

--- ClassWork_4/cw4.js
+++ ClassWork_4/cw4.js
@@ -8,7 +8,6 @@
 //         if (arguments[i] < min) min = arguments[i];
 //         if (arguments[i] > max) max = arguments[i];
 //     }
-//     console.log('min:', min);
 //     console.log('max:', max);
 //     return min;
 // }
@@ -17,8 +16,36 @@
 // - Взяти задачі з завдання 10 та 9 та обєднати їх в одну динамічну функцію.
 //     Що б я міг сам вибрати повернути мені масив ключів чи масив значень.
 
-// keysOrValues([{name: 'Dima', age: 13}, {model: 'Camry'}]);
+// function keyOrValues(object, keysOrValues= '') {
+//     let array = [];
+//     const length = object.length;
 //
+//     if (keysOrValues === 'keys') {
+//         for (let i = 0; i < length; i++) {
+//             for (const item in object[i]) {
+//                 array.push(item);
+//             }
+//         }
+//         console.log(array);
+//         return array;
+//     }
+//     if (keysOrValues === 'values') {
+//         for (let i = 0; i < length; i++) {
+//             for (const newArr in object[i]) {
+//                 array.push(object[i][newArr])
+//             }
+//         }
+//         console.log(array);
+//         return array;
+//     }
+// }
+//
+// keyOrValues([{name: 'Dima', age: 13}, {model: 'Camry'}], 'keys');
+// keyOrValues([{name: 'Dima', age: 13}, {model: 'Camry'}], 'values');
+
+//==============================================
+// Варіант із промпт:
+
 // function keysOrValues(mix) {
 //
 //     let array = [];
@@ -51,13 +78,42 @@
 //   foo([9,8,0,4], 0) // ==> [ 8, 9, 0, 4 ]
 //   foo([9,8,0,4], 1) // ==> [ 9 ,0, 8, 4 ]
 //   foo([9,8,0,4], 2) // ==> [ 9, 8, 4, 0 ]
+
+// function foo(array, i) {
+//     if(i < array.length){
+//         let change = array[i];
+//         array[i] = array [i+1]
+//         array[i+1] = change;
 //
+//         console.log(array);
+//     }
+// }
+// foo([9,8,0,4],2)
+
 // - Сворити функцію яка буде переносити елементи з значенням 0 у кінець маисву. Зберігаючи при цьому порядок не нульових значень.
 // Двожина масиву від 2 до 100
 // EXAMPLE:
 // [1,0,6,0,3] => [1,6,3,0,0]
 // [0,1,2,3,4] => [1,2,3,4,0]
 // [0,0,1,0]   => [1,0,0,0]
+
+// function transferZero(array) {
+// let zero = [];
+// let numbers = [];
+//     for (let i = 0; i < array.length; i++) {
+//        if (array[i] === 0) {
+//            zero.push(array[i]);
+//        }
+// if (!!array[i]) {
+//     numbers.push(array[i]);
+// }
+//     }
+// return numbers.concat(zero);
+// }
+// console.log(transferZero([1,0,6,0,3]));
+// console.log(transferZero([0,1,2,3,4]));
+// console.log(transferZero([0,0,1,0]));
+
 //
 // - Дано список імен.
 // let n1 = '    Harry       Potter      '
@@ -67,6 +123,18 @@
 // let n1 = 'Harry Potter'
 // let n2 = 'Ron Whisley'
 // let n3 = 'Hermione Granger'
+
+
+// function validator(name) {
+//     name = name.trim();
+//     name = name.replace(/ +/g, ' ');
+//     console.log(name);
+// }
+//
+// validator('    Harry       Potter      ');
+// validator('    Ron       Whisley      ');
+// validator('    Hermione       Granger      ');
+
 //
 // ============================================================================================
 //
