8e1539e1f8de2679d7951eaca1ea03437d5e33a8
shumynvolodymyr <shumivova@gmail.com>
2021-04-09 00:40:27 +0300

    Homework_6

--- Homework_6/script.js
+++ Homework_6/script.js
@@ -1,33 +1,29 @@
 // 1) Створити масив з 20 чисел та:
 //     a) відсортувати його від меншого до більшого.
 
-// let numbers = [1,3,8,13,0,4,5,25,35,19,11,7,4,5,8,9,4,14,16,18];
+// let numbers = [1, 3, 8, 13, 0, 4, 5, 25, 35, 19, 11, 7, 4, 5, 8, 9, 4, 14, 16, 18];
 
-// let numbersOne = numbers.sort((a,b) => {return a-b});
+// let numbersOne = numbers.sort((a,b) => a-b);
 // console.log(numbersOne);
 
 //     b) відсортувати його від більшого до меншого.
 
-// let numbersTwo = numbers.sort((a,b)=> {return b-a});
+// let numbersTwo = numbers.sort((a, b) => b - a);
 // console.log(numbersTwo);
 
 //     c) Відфілтрувати числа які є кратними 3.
 
-// let numbersThree = numbers.filter((value) => {
-//     return value % 3 === 0;
-// });
+// let numbersThree = numbers.filter((a) => a % 3 === 0);
 // console.log(numbersThree);
 
 // d) Відфілтрувати числа які є більшими за 10.
 
-// let numbersFour = numbers.filter((value) =>{
-//     return value > 10;
-// });
+// let numbersFour = numbers.filter((a) => a > 10);
 // console.log(numbersFour);
 
 // e) Проітерувати його forEach та вивести кожеш елмент за допомогою document.write
 
-// numbers.forEach((value) => document.write(`${value} <br>`));
+// numbers.forEach((a) => document.write(` ${a} <hr>`));
 
 // f) За допомогою map збільшити кожен елемент в масиві в три рази.
 
@@ -41,12 +37,17 @@
 
 // 2) Створити масив з 20 стрічок та:
 
-// let users = ['One', 'Two','Пять', 'Шість', 'Сім' , 'Three', 'Four', 'Вісім','David', 'Девять', 'Десять', 'Bara', 'kara','Fara','Dasha','Masha', 'Sasha', 'Natasha', 'Artem'];
+// let users = ['One', 'Two','Пять','karIna', 'Шість', 'Сім' , 'Three', 'four', 'Вісім','David', 'Девять', 'десять', 'Bara', 'kara','Fara','Dasha','Masha', 'Sasha', 'Natasha', 'Artem'];
 // console.log(users);
 
 // a) Відсортувати його в алфавітному порядку
 
-// let user = users.sort();
+// let user = users.sort((a ,b)=> {
+//    let nameA = a.toLowerCase();
+//    let nameB = b.toLowerCase();
+//    if (nameA<nameB) return -1;
+//    if (nameA>nameB) return 1;
+// });
 // console.log(user);
 
 // b) Відсортувати в зворотньому порядку
@@ -70,19 +71,19 @@
 
 // 3) Все робити тільки за допомогою методів масивів!
 //     Дано масив :
-    const users = [
-        {name: 'vasya', age: 31, isMarried: false},
-        {name: 'petya', age: 30, isMarried: true},
-        {name: 'kolya', age: 29, isMarried: true},
-        {name: 'olya', age: 28, isMarried: false},
-        {name: 'max', age: 30, isMarried: true},
-        {name: 'anya', age: 31, isMarried: false},
-        {name: 'oleg', age: 28, isMarried: false},
-        {name: 'andrey', age: 29, isMarried: true},
-        {name: 'masha', age: 30, isMarried: true},
-        {name: 'olya', age: 31, isMarried: false},
-        {name: 'max', age: 31, isMarried: true}
-    ];
+//     const users = [
+//         {name: 'vasya', age: 31, isMarried: false},
+//         {name: 'petya', age: 30, isMarried: true},
+//         {name: 'kolya', age: 29, isMarried: true},
+//         {name: 'olya', age: 28, isMarried: false},
+//         {name: 'max', age: 30, isMarried: true},
+//         {name: 'anya', age: 31, isMarried: false},
+//         {name: 'oleg', age: 28, isMarried: false},
+//         {name: 'andrey', age: 29, isMarried: true},
+//         {name: 'masha', age: 30, isMarried: true},
+//         {name: 'olya', age: 31, isMarried: false},
+//         {name: 'max', age: 31, isMarried: true}
+//     ];
 // a) відсортувати його за  віком (зростання , а потім окремо спадання)
 
 // let user = users.sort((a,b)=> a.age-b.age);
@@ -101,16 +102,10 @@
 
 // c) пройтись по ньому та додати кожному юзеру поле id - яке характеризує унікальний індентифікатор (По якому принципу його створювати - ваше рішення),
 // та зберегти це в новий масив (первинний масив залишиться без змін)
-
-// let usersId = users.map((name, idndex) => {
-//    return {
-//        id: idndex +1,
-//        name
-//    }
-// });
-
 // d) відсортувати його за індентифікатором
 
+// let usersId = users.map((name, idndex) => ({id: idndex +1, ...name}));
+//
 // usersId.sort((a,b)=> b.id - a.id);
 // console.log(usersId);
 
@@ -127,7 +122,7 @@
 // console.log(flat);
 
 
- // ______________________________________________________________________________________________________________________________________________________
+// ______________________________________________________________________________________________________________________________________________________
 // РОБОТА В АУДИТОРІЇ
 // ______________________________________________________________________________________________________________________________________________________
 //
@@ -144,52 +139,135 @@
 //     {producer:"bmw",model: "650",year: 2009, color:"black",type: "coupe",engine: "f60",volume: 6,power: 350},
 //     {producer:"bmw",model: "320",year: 2012, color:"red",type: "sedan",engine: "f20",volume: 2,power: 180},
 //     {producer:"mercedes",model: "e200",year: 1990, color:"silver",type: "sedan",engine: "eng200",volume: 2,power: 180},
-//     {producer:"mercedes",model: "e63",year 2017, color:"yellow",type: "sedan",engine: "amg63",volume:3,power: 400},
+//     {producer:"mercedes",model: "e63",year: 2017, color:"yellow",type: "sedan",engine: "amg63",volume:3,power: 400},
 //     {producer:"mercedes",model: "c250",year: 2017, color:"red",type: "sedan",engine: "eng25",volume: 2.5,power: 230}
 // ];
 //
 // Відфільтрувати масив за наступними крітеріями :
 //     - двигун більше 3х літрів
+
+// let result = cars.filter((values) => values.volume > 3);
+// console.log(result);
+
 // - двигун = 2л
+
+// console.log(cars.filter((values) => values.volume === 2));
+
 // - виробник мерс
+
+// console.log(cars.filter((values) => values.producer === 'mercedes'));
+
 // - двигун більше 3х літрів + виробник мерседес
+
+// console.log(cars.filter((values) => values.volume === 3 && values.producer === 'mercedes'));
+
 // - двигун більше 3х літрів + виробник субару
+
+// console.log(cars.filter((values)=> values.volume >= 3 && values.producer === 'subaru'));
+
 // - сили більше ніж 300
+
+// console.log(cars.filter((value) => value.power > 300));
+
 // - сили більше ніж 300 + виробник субару
+
+// console.log((cars.filter((values) => values.power > 300 && values.producer === 'subaru')));
+
 // - мотор серіі ej
+
+// console.log(cars.filter(value => value.engine.startsWith('ej')));
+
 // - сили більше ніж 300 + виробник субару + мотор серіі ej
+
+// console.log((cars.filter(value => value.power > 300 && value.producer === 'subaru' && value.engine.startsWith('ej'))));
+
 // - двигун меньше 3х літрів + виробник мерседес
+
+// console.log((cars.filter(value => value.volume < 3 && value.producer === 'mercedes')));
+
 // - двигун більше 2л + сили більше 250
+
+// console.log(cars.filter(value => value.volume > 2 && value.power > 250));
+
 // - сили більше 250  + виробник бмв
-//
-//
+
+// console.log((cars.filter(value => value.power > 250 && value.producer === 'bmw')));
+
 // - взять слдующий массив
-// const usersWithAddress = [
-//     {id: 9, name: 'vasya', age: 31, isMarried: false, address: {city: 'Kyiv', street: 'Gongadze', number: 16}},
-//     {id: 2, name: 'petya', age: 30, isMarried: true, address: {city: 'Rivne', street: 'Zelena', number: 1}},
-//     {id: 4, name: 'kolya', age: 29, isMarried: true, address: {city: 'Lviv', street: 'Pasichna', number: 121}},
-//     {id: 3, name: 'olya', age: 28, isMarried: false, address: {city: 'Rivne', street: 'Shevchenko', number: 90}},
-//     {id: 8, name: 'max', age: 30, isMarried: true, address: {city: 'Lviv', street: 'Kriva Lipa', number: 115}},
-//     {id: 6, name: 'anya', age: 31, isMarried: false, address: {city: 'Lviv', street: 'Shevchenko', number: 2}},
-//     {id: 10, name: 'oleg', age: 28, isMarried: false, address: {city: 'Kyiv', street: 'Centralna', number: 22}},
-//     {id: 5, name: 'andrey', age: 29, isMarried: true, address: {city: 'Lviv', street: 'Gorodotska', number: 43}},
-//     {id: 1, name: 'masha', age: 30, isMarried: true, address: {city: 'Kyiv', street: 'Peremogi', number: 12}},
-//     {id: 7, name: 'olya', age: 31, isMarried: false, address: {city: 'Lviv', street: 'Naukova', number: 16}},
-//     {id: 11, name: 'max', age: 31, isMarried: true, address: {city: 'Rivne', street: 'Ivana Franka', number: 121}}
-// ];
-//
+const usersWithAddress = [
+    {id: 9, name: 'vasya', age: 31, isMarried: false, address: {city: 'Kyiv', street: 'Gongadze', number: 16}},
+    {id: 2, name: 'petya', age: 30, isMarried: true, address: {city: 'Rivne', street: 'Zelena', number: 1}},
+    {id: 4, name: 'kolya', age: 29, isMarried: true, address: {city: 'Lviv', street: 'Pasichna', number: 121}},
+    {id: 3, name: 'olya', age: 28, isMarried: false, address: {city: 'Rivne', street: 'Shevchenko', number: 90}},
+    {id: 8, name: 'max', age: 30, isMarried: true, address: {city: 'Lviv', street: 'Kriva Lipa', number: 115}},
+    {id: 6, name: 'anya', age: 31, isMarried: false, address: {city: 'Lviv', street: 'Shevchenko', number: 2}},
+    {id: 10, name: 'Oleg', age: 28, isMarried: false, address: {city: 'Kyiv', street: 'Centralna', number: 22}},
+    {id: 5, name: 'andrey', age: 29, isMarried: true, address: {city: 'Lviv', street: 'Gorodotska', number: 43}},
+    {id: 1, name: 'masha', age: 30, isMarried: true, address: {city: 'Kyiv', street: 'Peremogi', number: 12}},
+    {id: 7, name: 'olya', age: 31, isMarried: false, address: {city: 'Lviv', street: 'Naukova', number: 16}},
+    {id: 11, name: 'max', age: 31, isMarried: true, address: {city: 'Rivne', street: 'Ivana Franka', number: 121}}
+];
+
 // -- Відсортувати їх по ID
+
+// console.log((usersWithAddress.sort((a, b) => a.id - b.id)));
+
 // -- Відсортувати їх по ID в зворотньому порядку
+
+// console.log((usersWithAddress.sort((a, b) => b.id - a.id)));
+
 // -- Відсортувати по віку
+
+// console.log(usersWithAddress.sort((a,b)=> a.age - b.age));
+
 // -- Відсортувати по віку в зворотньому порядку
+
+// console.log(usersWithAddress.sort((a,b)=> b.age - a.age));
+
 // -- Відсорутвати по імені
+
+// console.log(usersWithAddress.sort((a,b)=> {
+//     let nameA = a.name.toUpperCase();
+//     let nameB = b.name.toUpperCase();
+//     if (nameA < nameB) return -1;
+//     if (nameA > nameB) return 1;
+// }));
+
 // -- Відсорутвати по назві вулиці
+
+// console.log(usersWithAddress.sort((a,b)=> {
+//     let streetA = a.address.street.toUpperCase();
+//     let streetB = b.address.street.toUpperCase();
+//     if (streetA < streetB) return -1;
+//     if (streetA > streetB) return 1;
+// }));
+
 // -- Відсорутвати по номеру будинку
+
+// console.log(usersWithAddress.sort((a,b)=> a.address.number - b.address.number));
+
 // -- Залигити тільки тих, хто молодший ніж 30 (filter)
+
+// console.log((usersWithAddress.filter((a) => a.age < 30)));
+
 // -- Залишити тільки одружених
+
+// console.log((usersWithAddress.filter((a) => a.isMarried === true)));
+
 // -- Залишити тільки одружених, які молодні за 30
+
+// console.log((usersWithAddress.filter((a) => a.isMarried === true && a.age < 30)));
+
 // -- Залишити лише тих, в кого парні номери будинків.
+
+// console.log((usersWithAddress.filter((a) => a.address.number % 2 === 0)));
+
 // -- Порахувати загальний вік всіх людей. (reduce)
+
+// console.log((usersWithAddress.reduce((acc, currentValue) => {
+//     return acc + currentValue.age;
+// }, 0)));
+
 // -- Ті, хто одружений і старий за 30 має отримати обєкти child і попасти в новий масив (reduce)
 //
 //
